79a4d20ad27696eba5fa515dc84bd1b51f968f0b
Simonemarchica1 <marchica.simone99@gmail.com>
2024-06-12 16:21:24 +0200

    ciao

---
+++ demoJakarta/src/main/java/com/example/demojakarta/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.demojakarta;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
